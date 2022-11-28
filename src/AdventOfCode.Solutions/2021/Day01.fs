1ce4b57476307a48a3570fa41419701612dcd140
Bart van Nierop <bvnierop@users.noreply.github.com>
2022-11-28 11:24:01 +0100

    Reorder / Solve 2021 Day 1

---
+++ src/AdventOfCode.Solutions/2021/Day01.fs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode.Solutions.Year2021
+
+open System
+
+module Day01 =
+    let solve1 () =
+        let values = System.IO.File.ReadAllLines("input/2021/01.in") |> Array.map Int32.Parse |> Array.toSeq
+        Seq.zip values (Seq.skip 1 values)
+        |> Seq.where (fun (a, b) -> a < b)
+        |> Seq.length
+        
+    let solve2 () =
+        let values = System.IO.File.ReadAllLines("input/2021/01.in") |> Array.map Int32.Parse |> Array.toSeq
+        Seq.zip values (Seq.skip 3 values)
+        |> Seq.filter (fun (a, b) -> a < b)
+        |> Seq.length
---
+++ src/AdventOfCode/Program.fs
@@ -0,0 +1,5 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+printfn "%d" (AdventOfCode.Solutions.Year2021.Day01.solve1 ())
+printfn "%d" (AdventOfCode.Solutions.Year2021.Day01.solve2 ())
