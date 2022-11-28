0acb15560b32d824a48f22159e15c62115830727
Bart van Nierop <bvnierop@users.noreply.github.com>
2022-11-28 12:04:22 +0100

    Solve 2018, Day 2

---
+++ src/AdventOfCode.Solutions/2018/Day02.fs
@@ -0,0 +1,28 @@
+namespace AdventOfCode.Solutions._2018
+
+module Day02 =
+    
+    let containsAnyNTimes n seq =
+        Seq.countBy id seq
+        |> Seq.exists (fun (_chr, count) -> count = n)
+        
+    let solve1 (input: string list) =
+        let twos = List.filter (containsAnyNTimes 2) input |> List.length
+        let threes = List.filter (containsAnyNTimes 3) input |> List.length
+        twos * threes
+
+    let strDist s1 s2 =
+        Seq.zip s1 s2
+        |> Seq.filter (fun (a, b) -> a <> b)
+        |> Seq.length
+        
+    let strCommon (s1, s2) =
+        Seq.zip s1 s2
+        |> Seq.choose (fun (a, b) -> if a = b then Some a else None)
+        |> Seq.toArray
+        |> System.String
+        
+    let solve2 (input: string list) =
+        let pairs = Seq.allPairs input input
+        let ones = Seq.filter (fun (s1, s2) -> strDist s1 s2 = 1) pairs
+        strCommon (Seq.head ones)
--- src/AdventOfCode/Program.fs
+++ src/AdventOfCode/Program.fs
@@ -3,3 +3,8 @@ printfn "Hello from F#"
 
 printfn "%d" (AdventOfCode.Solutions.Year2021.Day01.solve1 ())
 printfn "%d" (AdventOfCode.Solutions.Year2021.Day01.solve2 ())
+
+let input201802 = System.IO.File.ReadAllLines("input/2018/02.in") |> Array.toList
+
+printfn "%d" (AdventOfCode.Solutions._2018.Day02.solve1 input201802)
+printfn "%s" (AdventOfCode.Solutions._2018.Day02.solve2 input201802)
