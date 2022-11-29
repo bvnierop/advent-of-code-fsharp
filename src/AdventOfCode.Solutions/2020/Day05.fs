885df5676708e7615315098590eaaa1bdf82ab81
Bart van Nierop <bvnierop@users.noreply.github.com>
2022-11-29 20:08:27 +0100

    Solve 2020, day 5

---
+++ src/AdventOfCode.Solutions/2020/Day05.fs
@@ -0,0 +1,27 @@
+namespace AdventOfCode.Solutions._2020
+
+open AdventOfCode.Solutions.Solver
+open System
+
+module Day05 =
+    let parseSeat (str: string) =
+        let bStr = str.Replace('F', '0')
+                      .Replace('B', '1')
+                      .Replace('L', '0')
+                      .Replace('R', '1')
+        Convert.ToInt32(bStr, 2)
+        
+    [<AocSolver(2020, 5, Level = 1)>]
+    let solve1 (input: string list) =
+        input
+        |> List.map parseSeat
+        |> List.max
+        
+    [<AocSolver(2020, 5, Level = 2)>]
+    let solve2 (input: string list) =
+        let sorted = input
+                     |> List.map parseSeat
+                     |> List.sort
+        let (a, _b) = Seq.zip sorted (Seq.skip 1 sorted)
+                      |> Seq.find (fun (a, b) -> b - a = 2)
+        a + 1
