425eb637a1ae4fb941f647255b9ea43c5ddd33be
Bart van Nierop <bvnierop@users.noreply.github.com>
2022-12-02 08:10:43 +0100

    Refactor 2022-02 to a more functional style

--- src/AdventOfCode.Solutions/2022/Day02.fs
+++ src/AdventOfCode.Solutions/2022/Day02.fs
@@ -6,34 +6,30 @@ open System
 module Day02 =
     [<AocSolver(2022, 2, Level = 1)>]
     let solve1 (input: string list) =
-        let mutable score = 0
-        for line in input do
+        List.fold (fun score (line: string) ->
             match line.Split(" ") with
-            | [|"A"; "X"|] -> score <- score + 1 + 3
-            | [|"A"; "Y"|] -> score <- score + 2 + 6
-            | [|"A"; "Z"|] -> score <- score + 3 + 0
-            | [|"B"; "X"|] -> score <- score + 1 + 0
-            | [|"B"; "Y"|] -> score <- score + 2 + 3
-            | [|"B"; "Z"|] -> score <- score + 3 + 6
-            | [|"C"; "X"|] -> score <- score + 1 + 6
-            | [|"C"; "Y"|] -> score <- score + 2 + 0
-            | [|"C"; "Z"|] -> score <- score + 3 + 3
-            | _ -> failwith "Impossible move"
-        score
+            | [|"A"; "X"|] -> score + 1 + 3
+            | [|"A"; "Y"|] -> score + 2 + 6
+            | [|"A"; "Z"|] -> score + 3 + 0
+            | [|"B"; "X"|] -> score + 1 + 0
+            | [|"B"; "Y"|] -> score + 2 + 3
+            | [|"B"; "Z"|] -> score + 3 + 6
+            | [|"C"; "X"|] -> score + 1 + 6
+            | [|"C"; "Y"|] -> score + 2 + 0
+            | [|"C"; "Z"|] -> score + 3 + 3
+            | _ -> failwith "Impossible move") 0 input
         
     [<AocSolver(2022, 2, Level = 2)>]
     let solve2 (input: string list) =
-        let mutable score = 0
-        for line in input do
+        List.fold (fun score (line: string) ->
             match line.Split(" ") with
-            | [|"A"; "X"|] -> score <- score + 3 + 0
-            | [|"A"; "Y"|] -> score <- score + 1 + 3
-            | [|"A"; "Z"|] -> score <- score + 2 + 6
-            | [|"B"; "X"|] -> score <- score + 1 + 0
-            | [|"B"; "Y"|] -> score <- score + 2 + 3
-            | [|"B"; "Z"|] -> score <- score + 3 + 6
-            | [|"C"; "X"|] -> score <- score + 2 + 0
-            | [|"C"; "Y"|] -> score <- score + 3 + 3
-            | [|"C"; "Z"|] -> score <- score + 1 + 6
-            | _ -> failwith "Impossible move"
-        score
+            | [|"A"; "X"|] -> score + 3 + 0
+            | [|"A"; "Y"|] -> score + 1 + 3
+            | [|"A"; "Z"|] -> score + 2 + 6
+            | [|"B"; "X"|] -> score + 1 + 0
+            | [|"B"; "Y"|] -> score + 2 + 3
+            | [|"B"; "Z"|] -> score + 3 + 6
+            | [|"C"; "X"|] -> score + 2 + 0
+            | [|"C"; "Y"|] -> score + 3 + 3
+            | [|"C"; "Z"|] -> score + 1 + 6
+            | _ -> failwith "Impossible move") 0 input
