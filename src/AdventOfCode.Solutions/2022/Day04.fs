1ecdfed0593bbd223cf8954f27acf28fbea17d1b
Bart van Nierop <bvnierop@users.noreply.github.com>
2022-12-04 21:26:19 +0100

    Solve 2022-04 part 2 with not disjoint

--- src/AdventOfCode.Solutions/2022/Day04.fs
+++ src/AdventOfCode.Solutions/2022/Day04.fs
@@ -26,12 +26,10 @@ module Day04 =
         | ((l1, h1), (l2, h2)) when l2 <= l1 && h2 >= h1 -> true // range2 fully contains range 1
         | _ -> false
         
-    let isPartlyOverlapping range1 range2 = 
+    let isDisjoint range1 range2 =
         match (range1, range2) with
-        | ((l1, h1), (l2, h2)) when l1 <= l2 && l2 <= h1 -> true // range2 begins in range1
-        | ((l1, h1), (l2, h2)) when l1 <= h2 && h2 <= h1 -> true // range2 begins in range1
-        | ((l1, h1), (l2, h2)) when l2 <= l1 && l1 <= h2 -> true // range1 begins in range2
-        | ((l1, h1), (l2, h2)) when l2 <= h1 && h1 <= h2 -> true // range1 begins in range2
+        | ((l1, h1), (l2, h2)) when h2 < l1 -> true
+        | ((l1, h1), (l2, h2)) when h1 < l2 -> true
         | _ -> false
         
     [<AocSolver(2022, 4, Level = 1)>]
@@ -40,10 +38,10 @@ module Day04 =
         |> List.map parse
         |> List.filter (fun (r1, r2) -> isFullyOverlapping r1 r2)
         |> List.length
-        
+    
     [<AocSolver(2022, 4, Level = 2)>]
     let solve2 (input: string list) =
         input
         |> List.map parse
-        |> List.filter (fun (r1, r2) -> isPartlyOverlapping r1 r2)
+        |> List.filter (fun (r1, r2) -> not (isDisjoint r1 r2))
         |> List.length
