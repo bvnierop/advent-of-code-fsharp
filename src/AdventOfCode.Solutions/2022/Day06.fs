34a06a843b10030d9fa72119e3867aa0dbed0a5a
Bart van Nierop <bvnierop@users.noreply.github.com>
2022-12-06 06:30:39 +0100

    2022-06: Extract solver method

--- src/AdventOfCode.Solutions/2022/Day06.fs
+++ src/AdventOfCode.Solutions/2022/Day06.fs
@@ -4,16 +4,16 @@ open AdventOfCode.Lib.Solver
 open System
 
 module Day06 =
+    let findFirstUniqueSubsequence n seq =
+        seq
+        |> Seq.windowed n
+        |> Seq.findIndex (fun window -> Seq.distinct window |> Seq.length = n)
+        |> ((+) n)
+        
     [<AocSolver(2022, 6, Level = 1)>]
     let solve1 (input: string) =
-        input
-        |> Seq.windowed 4
-        |> Seq.findIndex (fun window -> Seq.distinct window |> Seq.length = 4)
-        |> ((+) 4)
+        findFirstUniqueSubsequence 4 input
         
     [<AocSolver(2022, 6, Level = 2)>]
     let solve2 (input: string) =
-        input
-        |> Seq.windowed 14
-        |> Seq.findIndex (fun window -> Seq.distinct window |> Seq.length = 14)
-        |> ((+) 14)
+        findFirstUniqueSubsequence 14 input
