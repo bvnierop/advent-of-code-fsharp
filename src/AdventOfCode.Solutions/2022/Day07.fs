34392d323660e54a3d51bccd1ee81137e862d2b4
Bart van Nierop <bvnierop@users.noreply.github.com>
2022-12-07 18:51:49 +0100

    Refactor 2022-07

--- src/AdventOfCode.Solutions/2022/Day07.fs
+++ src/AdventOfCode.Solutions/2022/Day07.fs
@@ -13,32 +13,38 @@ module Day07 =
                 |> Map.change pwd (fun i -> Some (Option.defaultValue 0 i + size))
             updateSizes xs size updated
     let calculateSizes (lines: string list) =
-        let mutable pwd = []
-        let mutable sizes = Map.empty
-        for line in lines do
+        let handleLine (pwd, sizes) (line: string) = 
             match line.Split(" ") with
-            | [|"$"; "cd"; ".."|] -> pwd <- List.tail pwd
-            | [|"$"; "cd"; dirName|] -> pwd <- dirName :: pwd
-            | [|"$"; "ls"|] -> ()
-            | [|"dir"; _|] -> ()
-            | [|sizeStr; _|] when sizeStr <> "dir" ->
+            | [|"$"; "cd"; ".."|] -> (List.tail pwd, sizes)
+            | [|"$"; "cd"; dirName|] -> (dirName :: pwd, sizes)
+            | [|"$"; "ls"|] -> (pwd, sizes)
+            | [|"dir"; _|] -> (pwd, sizes)
+            | [|sizeStr; _|] ->
                 let size = Int32.Parse(sizeStr)
-                sizes <- updateSizes pwd size sizes 
+                (pwd, updateSizes pwd size sizes)
             | _ -> failwith "Failed to parse"
+            
+        let (_, sizes) =
+            lines
+            |> List.fold handleLine ([], Map.empty)
+            
         sizes
             
     [<AocSolver(2022, 7, Level = 1)>]
     let solve1 (input: string list) =
-        let sizes = calculateSizes input
-        let smallEnough = Map.filter (fun _ size -> size <= 100000) sizes 
-        Map.fold (fun sum _ size -> sum + size) 0 smallEnough 
+        input
+        |> calculateSizes
+        |> Map.values
+        |> Seq.filter (fun sz -> sz <= 100000)
+        |> Seq.sum
         
     [<AocSolver(2022, 7, Level = 2)>]
     let solve2 (input: string list) =
         let sizes = calculateSizes input
         let availableSpace = 70000000 - sizes[["/"]]
-        let spaceNeeded = 30000000 - availableSpace;
+        let spaceNeeded = 30000000 - availableSpace
+        
         sizes
         |> Map.values
-        |> Seq.filter (fun sz -> sz >= spaceNeeded)
+        |> Seq.filter (fun sz -> spaceNeeded <= sz)
         |> Seq.min
