badff799909fdad51634d03cd736ace64311a336
Bart van Nierop <bvnierop@users.noreply.github.com>
2022-12-10 11:27:09 +0100

    Solve 2022-10-2

--- src/AdventOfCode.Solutions/2022/Day10.fs
+++ src/AdventOfCode.Solutions/2022/Day10.fs
@@ -42,4 +42,42 @@ module Day10 =
         
     [<AocSolver(2022, 10, Level = 2)>]
     let solve2 (input: string list) =
-        2
+        // We have to divide the sequence by 240
+        // Not really, but at least we need an index
+        // We need to do something given an instruction and knowing how many instructions we have
+        //  We once again want to SCAN because we need intermediate results
+        // Given an instruction
+        //   - its index % 240 gives us the index in the display
+        //   - its X value determines the location of the sprite
+        //   - its index % 40 gives us the index in the _line_ of the display
+        //   So
+        //     We make an array of size 240
+        //     We update index % 240
+        //       # if X - 1 <= lineIndex <= X + 1
+        //       . otherwise
+        // Then when we have the display
+        //  We split it into 6
+        //  And write each line
+        let xValues =
+            xValues input
+            |> Seq.map (fun (x, _, _) -> x)
+            |> Seq.indexed
+        
+        let update display (index, x) =
+            let displayIndex = index % 240
+            let spriteIndex = x
+            let lineIndex = index % 40
+            if lineIndex >= spriteIndex - 1 && lineIndex <= spriteIndex + 1 then
+                Array.updateAt displayIndex '#' display
+            else
+                Array.updateAt displayIndex '.' display
+        
+        let display = Array.create 240 '.'
+        
+        xValues
+        |> Seq.scan update display
+        |> Seq.last
+        |> Array.splitInto 6
+        |> Array.map String.Concat
+        |> String.joinSeq Environment.NewLine
+            
