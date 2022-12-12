88ed503d62fd0515bac9d92e1612de1ff0a92cc9
Bart van Nierop <bvnierop@users.noreply.github.com>
2022-12-12 17:21:31 +0100

    2022-11: Generalize into one function

--- src/AdventOfCode.Solutions/2022/Day11.fs
+++ src/AdventOfCode.Solutions/2022/Day11.fs
@@ -7,8 +7,8 @@ module Day11 =
     
     type Monkey = {
         Items: int64 list
-        Operation: (string * int64);
-        Test: int64;
+        Operation: (string * int);
+        Test: int;
         IfTrue: int;
         IfFalse: int;
     }
@@ -17,19 +17,18 @@ module Day11 =
             str |> String.split |> Array.rev |> Array.take 2
         match finalTwoElements with
         | [|num; op|] ->
-            if num = "old" then ("exp", 2L)
-            else (op, Int64.parse num)
+            if num = "old" then ("exp", 2)
+            else (op, Int32.parse num)
         | _ -> failwith $"Failed to parse operation: {str}"
         
     let parseMonkey (input: string list) =
-        let lastAsInt64 str = str |> String.split |> Array.last |> Int64.parse
         let lastAsInt str = str |> String.split |> Array.last |> Int32.parse
         match input |> List.skip 1 with
         | [items;operation;test;ifTrue;ifFalse] ->
             {
                 Items = items |> String.splitOn [|':'|] |> Array.last |> String.splitOn [|','|] |> Array.map Int64.parse |> Array.toList;
                 Operation = parseOperation operation;
-                Test = lastAsInt64 test;
+                Test = lastAsInt test;
                 IfTrue = lastAsInt ifTrue;
                 IfFalse = lastAsInt ifFalse;
             }
@@ -40,81 +39,8 @@ module Day11 =
         |> List.map parseMonkey
         |> List.toArray
         
-    [<AocSolver(2022, 11, Level = 1)>]
-    let solve1 (input: string list) =
-        let monkeys = parseMonkeys input
-        // First parse each monkey
-        //    Splitting the input on empty lines
-        //    Discard the first line (Monkey 0:)
-        //    The second line is the items, comma separated after a colon
-        //    Third line is the operation, we're only interested in the last two elements
-        //    Fourth line: test = divisible by last element
-        //      If true -> last element
-        //      If false -> last element
-        // Simulate a round
-        //   Note to self: Figure out later if we can use fold or scan or something like that
-        //   For each monkey in order
-        //    If it does have items
-        //      Process the monkey
-        //      Continue simulating the round with the next monkey
-        
-        let mutable counts = Array.create (Array.length monkeys) 0
-        
-        let processMonkey index (monkeys: Monkey array) =
-            let monkey = monkeys[index]
-            let mutable m = monkeys
-            let throwItem n item monkeys =
-                monkeys
-                |> Array.updateAt n { monkeys[n] with Items = List.append monkeys[n].Items [item] }
-                
-            let calculateWorry (operation: (string * int64)) item =
-                match operation with
-                | ("*", n) -> item * n
-                | ("+", n) -> item + n
-                | ("exp", _) -> item * item
-                | _ -> failwith "We messed up big time here"
-                
-            for item in monkey.Items do
-                let worry = (calculateWorry monkey.Operation item) / 3L
-                // Note: mark that the monkey has inspected an item
-                if worry % monkey.Test = 0 then m <- throwItem monkey.IfTrue worry m
-                else m <- throwItem monkey.IfFalse worry m
-                
-            counts <- Array.updateAt index (counts[index] + (List.length m[index].Items)) counts
-            Array.updateAt index { m[index] with Items = [] } m
-                
-        //       Increase worry level based on formula
-        //       Divide worry level by three
-        //       Mark that the monkey has inspected an item
-        //       Perform test -> Throw item
-        
-        let rec round n monkeys =
-            if n = Array.length monkeys then monkeys
-            else
-                round <| n + 1 <| processMonkey n monkeys
-            
-        let mutable m = monkeys
-        for i = 1 to 20 do
-            m <- round 0 m
-            
-        // Processing a monkey
-        //    For each item in the monkey's possession
-        //       Increase worry level based on formula
-        //       Mark that the monkey has inspected an item
-        //       Divide worry level by three
-        //       Perform test -> Throw item
-        // Simulate 20 rounds
-        // Sort monkeys by inspected items
-        // Take the top 2
-        // Multiply them together
-        counts |> Array.sortDescending |> Array.take 2 |> Array.fold (*) 1
-        
-    [<AocSolver(2022, 11, Level = 2)>]
-    let solve2 (input: string list) =
-        let monkeys = parseMonkeys input
-        
+    let solve rounds worryControlFn monkeys =
         let mutable counts = Array.create (Array.length monkeys) 0L
-        let product = monkeys |> Array.map (fun m -> m.Test) |> Array.fold (*) 1L
         
         let processMonkey index (monkeys: Monkey array) =
             let monkey = monkeys[index]
@@ -123,20 +49,20 @@ module Day11 =
                 monkeys
                 |> Array.updateAt n { monkeys[n] with Items = List.append monkeys[n].Items [item] }
                 
-            let calculateWorry (operation: (string * int64)) item =
+            let calculateWorry (operation: (string * int)) item =
                 match operation with
-                | ("*", n) -> item * n
-                | ("+", n) -> item + n
+                | ("*", n) -> item * int64 n
+                | ("+", n) -> item + int64 n
                 | ("exp", _) -> item * item
                 | _ -> failwith "We messed up big time here"
                 
             for item in monkey.Items do
                 let mutable worry = (calculateWorry monkey.Operation item)
-                worry <- worry % product
+                worry <- worryControlFn worry
                 assert(worry >= 0L)
                 
                 // Note: mark that the monkey has inspected an item
-                if worry % monkey.Test = 0 then m <- throwItem monkey.IfTrue worry m
+                if worry % int64 monkey.Test = 0 then m <- throwItem monkey.IfTrue worry m
                 else m <- throwItem monkey.IfFalse worry m
                 
             counts <- Array.updateAt index (counts[index] + (int64)(List.length m[index].Items)) counts
@@ -148,7 +74,18 @@ module Day11 =
                 round <| n + 1 <| processMonkey n monkeys
             
         let mutable m = monkeys
-        for i = 1 to 10000 do
+        for i = 1 to rounds do
             m <- round 0 m
             
         counts |> Array.sortDescending |> Array.take 2 |> Array.fold (*) 1L
+        
+    [<AocSolver(2022, 11, Level = 1)>]
+    let solve1 (input: string list) =
+        let monkeys = parseMonkeys input
+        solve 20 (fun w -> w / 3L) monkeys
+        
+    [<AocSolver(2022, 11, Level = 2)>]
+    let solve2 (input: string list) =
+        let monkeys = parseMonkeys input
+        let product = monkeys |> Array.map (fun m -> int64 m.Test) |> Array.fold (*) 1L
+        solve 10000 (fun w -> w % product) monkeys
