5eb585fc09fc5b1615b9e2e9003417ed10737d45
Bart van Nierop <bvnierop@users.noreply.github.com>
2022-12-19 22:03:39 +0100

    2022-14: Merge solutions

--- src/AdventOfCode.Solutions/2022/Day14.fs
+++ src/AdventOfCode.Solutions/2022/Day14.fs
@@ -10,45 +10,7 @@ module Day14 =
     let pLine = sepBy pPoint (pstring " -> ")
     let parse str = parseOrDie pLine str
     
-    [<AocSolver(2022, 14, Level = 1)>]
-    let solve1 (input: string list) =
-        let points ((x1, y1), (x2, y2)) = seq {
-            for x = min x1 x2 to max x1 x2 do
-                for y = min y1 y2 to max y1 y2 do
-                    yield (x, y)
-        }
-        
-        let rocks =
-            input |> List.map parse
-            |> List.map (List.pairwise >> List.map points >> Seq.concat)
-            |> Seq.concat
-            |> Set.ofSeq
-            
-        let low = rocks |> Seq.maxBy snd |> snd
-        
-        let rec addSand occupied threshold (atX, atY) =
-            let targets = [(atX, atY + 1); (atX - 1, atY + 1); (atX + 1, atY + 1)]
-            if atY >= threshold then occupied
-            else
-                let target =
-                    targets |> List.map (fun pt -> (pt, Set.contains pt occupied))
-                    |> List.tryFind (fun (pt, occ) -> occ = false)
-                match target with
-                | Some (pt, _) -> addSand occupied threshold pt
-                | None -> Set.add (atX, atY) occupied
-            
-        let rec simulate occupied =
-            let withSand = addSand occupied low (500, 0)
-            if Set.count occupied = Set.count withSand then occupied
-            else simulate withSand
-            
-        let withSand = simulate rocks
-        Set.count withSand - Set.count rocks
-        
-        
-        
-    [<AocSolver(2022, 14, Level = 2)>]
-    let solve2 (input: string list) =
+    let solver (input: string list) modifyFn =
         let points ((x1, y1), (x2, y2)) = seq {
             for x = min x1 x2 to max x1 x2 do
                 for y = min y1 y2 to max y1 y2 do
@@ -61,9 +23,7 @@ module Day14 =
             |> Seq.concat
             |> HashSet.ofSeq
             
-        let low = rocks |> HashSet.toSeq |> Seq.maxBy snd |> snd
-        
-        let rocksWithLowerLayer = [-100..1100] |> List.fold (fun occ x -> HashSet.add (x, low + 2) occ) rocks
+        let preppedRocks = modifyFn rocks
         
         let rec addSand occupied threshold (atX, atY) =
             let targets = [(atX, atY + 1); (atX - 1, atY + 1); (atX + 1, atY + 1)]
@@ -76,10 +36,23 @@ module Day14 =
                 | Some (pt, _) -> addSand occupied threshold pt
                 | None -> HashSet.add (atX, atY) occupied
             
+        let low = preppedRocks |> Seq.maxBy snd |> snd
         let rec simulate occupied =
-            let withSand = addSand occupied (low + 2) (500, 0)
+            let withSand = addSand occupied low (500, 0)
             if HashSet.size occupied = HashSet.size withSand then occupied
             else simulate withSand
             
-        let withSand = simulate rocksWithLowerLayer
-        HashSet.size withSand - HashSet.size rocksWithLowerLayer
+        let withSand = simulate preppedRocks
+        HashSet.size withSand - HashSet.size preppedRocks
+        
+    [<AocSolver(2022, 14, Level = 1)>]
+    let solve1 (input: string list) =
+        solver input id
+        
+    [<AocSolver(2022, 14, Level = 2)>]
+    let solve2 (input: string list) =
+        let modify rocks =
+            let low = rocks |> HashSet.toSeq |> Seq.maxBy snd |> snd
+            [-100..1100] |> List.fold (fun occ x -> HashSet.add (x, low + 2) occ) rocks
+            
+        solver input modify
