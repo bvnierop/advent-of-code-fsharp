dda68059f6471c898a23b7d897b3a5fdf3d6ed5e
Bart van Nierop <mail@bvnierop.nl>
2023-12-12 15:41:32 +0100

    2023/10: use single directions

--- src/AdventOfCode.Solutions/2023/Day10.fs
+++ src/AdventOfCode.Solutions/2023/Day10.fs
@@ -4,26 +4,23 @@ open AdventOfCode.Lib.Solver
 
 module Day10 =
     type PipeConnection =
-        | NorthSouth // Vertical
-        | EastWest // Horizontal
-        | NorthEast
-        | NorthWest
-        | SouthEast
-        | SouthWest
+        | North
+        | South
+        | East
+        | West
         | Start
-        | Nothing
 
     module PipeConnection =
         let ofChar =
             function
-            | '|' -> NorthSouth
-            | '-' -> EastWest
-            | 'L' -> NorthEast
-            | 'J' -> NorthWest
-            | '7' -> SouthWest
-            | 'F' -> SouthEast
-            | 'S' -> Start
-            | _ -> Nothing
+            | '|' -> [ North; South ]
+            | '-' -> [ East; West ]
+            | 'L' -> [ North; East ]
+            | 'J' -> [ North; West ]
+            | '7' -> [ South; West ]
+            | 'F' -> [ South; East ]
+            | 'S' -> [ Start ]
+            | _ -> []
 
     let parse input =
         input
@@ -33,38 +30,21 @@ module Day10 =
         |> Array2D.map PipeConnection.ofChar
 
     let findStart map =
-        match Array2D.findIndex (fun x -> x = PipeConnection.Start) map with
+        match Array2D.findIndex (fun x -> x = [ Start ]) map with
         | Some(x, y) -> (x, y)
         | None -> failwith "No start found"
 
+    let (|ConnectsToSouth|_|) list =
+        if List.contains South list then Some() else None
 
-    let (|ConnectsToSouth|_|) =
-        function
-        | PipeConnection.NorthSouth
-        | PipeConnection.SouthWest
-        | PipeConnection.SouthEast -> Some()
-        | _ -> None
+    let (|ConnectsToNorth|_|) list =
+        if List.contains North list then Some() else None
 
-    let (|ConnectsToNorth|_|) =
-        function
-        | PipeConnection.NorthSouth
-        | PipeConnection.NorthWest
-        | PipeConnection.NorthEast -> Some()
-        | _ -> None
+    let (|ConnectsToEast|_|) list =
+        if List.contains East list then Some() else None
 
-    let (|ConnectsToEast|_|) =
-        function
-        | PipeConnection.EastWest
-        | PipeConnection.SouthEast
-        | PipeConnection.NorthEast -> Some()
-        | _ -> None
-
-    let (|ConnectsToWest|_|) =
-        function
-        | PipeConnection.EastWest
-        | PipeConnection.SouthWest
-        | PipeConnection.NorthWest -> Some()
-        | _ -> None
+    let (|ConnectsToWest|_|) list =
+        if List.contains West list then Some() else None
 
     let determineShape map (x, y) =
         let neighbours =
@@ -72,36 +52,33 @@ module Day10 =
               (x + 1, y) // East
               (x, y + 1) // South
               (x - 1, y) ] // West
-            |> List.map (fun (xx, yy) -> Array2D.getDefault xx yy PipeConnection.Nothing map)
+            |> List.map (fun (xx, yy) -> Array2D.getDefault xx yy [] map)
 
         match neighbours with
-        | [ ConnectsToSouth; _; ConnectsToNorth; _ ] -> PipeConnection.NorthSouth
-        | [ _; ConnectsToWest; _; ConnectsToEast ] -> PipeConnection.EastWest
-        | [ ConnectsToSouth; ConnectsToWest; _; _ ] -> PipeConnection.NorthEast
-        | [ ConnectsToSouth; _; _; ConnectsToEast ] -> PipeConnection.NorthWest
-        | [ _; _; ConnectsToNorth; ConnectsToEast ] -> PipeConnection.SouthWest
-        | [ _; ConnectsToWest; ConnectsToNorth; _ ] -> PipeConnection.SouthEast
+        | [ ConnectsToSouth; _; ConnectsToNorth; _ ] -> [ North; South ]
+        | [ _; ConnectsToWest; _; ConnectsToEast ] -> [ East; West ]
+        | [ ConnectsToSouth; ConnectsToWest; _; _ ] -> [ North; East ]
+        | [ ConnectsToSouth; _; _; ConnectsToEast ] -> [ North; West ]
+        | [ _; _; ConnectsToNorth; ConnectsToEast ] -> [ South; East ]
+        | [ _; ConnectsToWest; ConnectsToNorth; _ ] -> [ South; West ]
         | _ -> failwith "Could not detect shape"
 
-    let shapeAt (x, y) (map: PipeConnection[,]) =
+    let shapeAt (x, y) (map: PipeConnection list[,]) =
         match map[y, x] with
-        | Start -> determineShape map (x, y)
+        | [ Start ] -> determineShape map (x, y)
         | other -> other
 
-    let next (curx, cury) prev (map: PipeConnection[,]) =
-        let rec next' (curx, cury) prev shape =
-            match shape with
-            | NorthSouth -> [ (curx, cury - 1); (curx, cury + 1) ]
-            | EastWest -> [ (curx - 1, cury); (curx + 1, cury) ]
-            | NorthEast -> [ (curx + 1, cury); (curx, cury - 1) ]
-            | NorthWest -> [ (curx - 1, cury); (curx, cury - 1) ]
-            | SouthEast -> [ (curx + 1, cury); (curx, cury + 1) ]
-            | SouthWest -> [ (curx - 1, cury); (curx, cury + 1) ]
-            // | Start -> next' (curx, cury) prev (determineShape map (curx, cury))
-            | _ -> failwith "Invalid shape for finding next"
+    let offset (curx, cury) =
+        function
+        | North -> (curx + 0, cury - 1)
+        | South -> (curx + 0, cury + 1)
+        | East ->  (curx + 1, cury + 0)
+        | West ->  (curx - 1, cury + 0)
+        | Start -> failwith "todo"
 
-        // next' (curx, cury) prev map[cury, curx]
-        next' (curx, cury) prev (shapeAt (curx, cury) map)
+    let next (curx, cury) prev (map: PipeConnection list[,]) =
+        shapeAt (curx, cury) map
+        |> List.map (offset (curx, cury))
         |> List.filter (fun (x, y) -> (x, y) <> prev)
         |> List.head
 
@@ -112,31 +89,42 @@ module Day10 =
             let nextx, nexty = next (curx, cury) prev map
 
             match map[nexty, nextx] with
-            | PipeConnection.Start -> (curx, cury) :: path
+            | [ Start ] -> (curx, cury) :: path
             | _ -> follow (nextx, nexty) (curx, cury) ((curx, cury) :: path)
 
         follow (next start start map) start [ start ]
+
     let findInnerArea map =
         let path = findLoop map |> Set.ofList
+
         let verticalPipeInLoop x y =
             let shape = shapeAt (x, y) map
+
             if Set.contains (x, y) path then
                 match shape with
-                | NorthSouth | NorthEast | NorthWest -> true
+                | ConnectsToNorth -> true
                 | _ -> false
-            else false
+            else
+                false
 
-        Array2D.foldi (fun (cellsInLoop, inLoop) y x  _shape ->
-            let inLoop' = if x = 0 then false else inLoop
-            if verticalPipeInLoop x y then (cellsInLoop, not inLoop')
-            elif inLoop' && not (Set.contains (x, y) path) then (Set.add (x, y) cellsInLoop, inLoop')
-            else (cellsInLoop, inLoop')) (Set.empty, false) map
-        |> fst |> Set.count
+        Array2D.foldi
+            (fun (cellsInLoop, inLoop) y x _shape ->
+                let inLoop' = if x = 0 then false else inLoop
+
+                if verticalPipeInLoop x y then
+                    (cellsInLoop, not inLoop')
+                elif inLoop' && not (Set.contains (x, y) path) then
+                    (Set.add (x, y) cellsInLoop, inLoop')
+                else
+                    (cellsInLoop, inLoop'))
+            (Set.empty, false)
+            map
+        |> fst
+        |> Set.count
 
     [<AocSolver(2023, 10, Level = 1)>]
     let solve1 (input: string list) =
         findLoop (parse input) |> List.length |> (fun x -> x / 2)
 
     [<AocSolver(2023, 10, Level = 2)>]
-    let solve2 (input: string list) =
-        findInnerArea (parse input)
+    let solve2 (input: string list) = findInnerArea (parse input)
